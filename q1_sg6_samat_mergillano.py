7962cd0120e94d41a7282742b79baee304d60341
MarshallMergillano <mamergillano2030@clc.pshs.edu.ph>
2026-09-04 22:07:09 +0800

    Update q1_sg6_samat_mergillano.py

--- q1_sg6_samat_mergillano.py
+++ q1_sg6_samat_mergillano.py
@@ -1,5 +1,15 @@
 class Lab:
-  def __int__(self,room_number):
-    self.room_number = 
-
+  def __init__(self,room_number):
+    self.room_number = room_number
+    
 class Technician:
+  def __init__(self,name, lab_obj):
+    self.name = name
+    self.assigned_lab = None
+    
+  def assign_lab(self, lab_obj):
+    self.assigned_lab = lab_obj
+
+chem_lab = Lab("302")
+mr_cruz = Technician("Mr.Cruz")
+print(mr_cruz.assign_lab(chem_lab))
