7f5506e1aa9b7583c912700bd73f9dca666ce0c4
MarshallMergillano <mamergillano2030@clc.pshs.edu.ph>
2026-09-18 00:44:39 +0800

    Update q1_sg7_samat_mergillano.py

--- q1_sg7_samat_mergillano.py
+++ q1_sg7_samat_mergillano.py
@@ -1,3 +1,16 @@
+class Glassware:
+  def __init__(self,kindofglassware = "Beaker"):
+    self.kindofglassware = kindofglassware
 
-  
-  
+class Beaker(Glassware):
+  def __init__(self,kindofglassware = "Beaker"):
+    super().__init__(kindofglassware)
+    
+class Tray:
+  def __init__(self):
+    self.tray = [Beaker() for i in range(5)]
+    
+myTray = Tray()
+print(f"Tray contains {len(myTray.tray)} beakers")
+del myTray
+print("Tray has been deleted")
