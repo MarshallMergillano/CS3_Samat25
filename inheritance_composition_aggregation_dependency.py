82ca455199b38653773b6e4b4f6367554af6452a
MarshallMergillano <mamergillano2030@clc.pshs.edu.ph>
2026-09-17 22:15:30 +0800

    Update inheritance_composition_aggregation_dependency.py

--- inheritance_composition_aggregation_dependency.py
+++ inheritance_composition_aggregation_dependency.py
@@ -31,4 +31,94 @@ ferry.move(15)
 yacht = Boat("yacht","Subid Yacht")
 yacht.move(17)
 
-# Composition
+
+# COMPOSITION
+class Nucleus:
+  def __init__(self):
+    print("Nucleus is created")
+
+class Mitochondria:
+  def __init__(self):
+    print("Mitochondria is created")
+
+  def providePower(self):
+    print("Mitochondria is powering the cell")
+
+class Cell:
+  def __init__(self):
+    print("Cell is created")
+    self.nucleus = Nucleus()
+    self.mitochondria = Mitochondria()
+
+def exist(self):
+  print("Cell is existing")
+  self.mitochondria.providePower()
+
+def __del__(self):
+  del self.nucleus
+  del self.mitochondria
+  print("Cell is gone")
+
+cellAtWork = Cell()
+cellAtWork.exist()
+print(cellAtWork)
+del cellAtWork
+anotherCell = Cell()
+print(anotherCell)
+
+
+
+# AGGREGATION
+class Sauce:
+  def __init__(self,name,taste):
+    self.name = name; self.taste = taste
+    print(self.name,"is ready")
+  def __del__(self):
+    print(self.name,"is ubos na")
+
+class Tusoktusok:
+  def __init__(self,name,sauce):
+    self.name = name
+    self.sauce = sauce
+    print(self.name,"is cooked and has",self.sauce.name)
+  def eat(self):
+    print("I am eating",self.name,"and it has",self.sauce.name)
+  def __del__(self):
+    print(self.name,"was thrown into the trash can")
+
+hotsauce = Sauce("Hot sauce","spicy")
+fishball = Tusoktusok("fishball", hotsauce)
+del fishball
+print(hotsauce.name)
+
+    
+# DEPENDENCY
+class Sauce:
+  def __init__(self,name,taste):
+    self.name = name; self.taste = taste
+    print(self.name,"is ready")
+  def __del__(self):
+    print(self.name,"is ubos na")
+
+class Tusoktusok:
+  def __init__(self,name,sauce = None):
+    self.name = name
+    self.sauce = sauce
+    print(self.name,"is cooked")
+    if self.sauce != None:
+      print("It is dipped in",self.sauce)
+  def eat(self):
+    print("I am eating", self.name)
+    if self.sauce != None:
+    print("It tastes",self.sauce.taste)
+  def dip(self,sauce):
+    self.sauce = sauce
+    print(self.name,"was dipped in",self.sauce)
+  def __del__(self):
+    print(self.name,"was thrown into the trash can")
+
+hotsauce = Sauce("Hot sauce","spicy")
+fishball = Tusoktusok("fishball", hotsauce)
+fishball.eat()
+kikiam = Tusoktusok("kikiam")
+kikiam.eat() ;kikiam.dip(hotsauce); kikiam eat()
