86822b7dcf54c3884601f3e49a5214354fae4b5e
MarshallMergillano <mamergillano2030@clc.pshs.edu.ph>
2026-09-25 04:30:50 +0800

    Update tusoktusok_game.py

--- q1/tusoktusok_game.py
+++ q1/tusoktusok_game.py
@@ -5,4 +5,17 @@ def __init__(self,name):
   self.name = name
 def dip(self,sauce):
   self.sauce = sauce
-def ea
+def eat(self):
+  print("I ate",self.name,"with",self.sauce.name,"and it tastes",self.sauce.taste)
+
+Class Sauce:
+  name = ""
+  taste = ""
+def __init__(self,name,taste):
+  self.name =name
+  self.taste = taste
+
+fishball = Tusoktusok("fishball")
+vinegar = Sauce("vinegar","sour")
+fishball.dip(vinegar)
+fishball.eat()
